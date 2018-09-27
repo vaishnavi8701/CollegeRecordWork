5f2c0c8948261c3c3d49a138718f6bd456a3ab9d
Nilesh Lund <nileshlund@gmail.com>
2018-09-27 19:26:34 +0530

    Solved Question (a)

---
+++ EX2a.c
@@ -0,0 +1,9 @@
+# include <stdio.h>
+int main()
+{
+    int RollNo;
+    printf("Enter Your Roll Number...");
+    scanf("%d", &RollNo);
+    printf("Hello, Your Roll Number Is...%d", RollNo);
+    return 0;
+}
