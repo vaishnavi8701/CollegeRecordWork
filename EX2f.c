47fc8f6ea26455683088fc55b37d3715b068174c
Nilesh2000 <nileshlund@gmail.com>
2018-10-02 11:04:21 +0530

    Commited Changes

--- EX2f.c
+++ EX2f.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-int main()
+void main()
 {
     int i, j;
     printf("\nEnter Two Integers i and j...");
     scanf("%d %d", &i, &j);
     int Round_Off=i+j-i%j;
     printf("The Rounded Off Integer Is...%d\n", Round_Off);
-    return 0;
 }
