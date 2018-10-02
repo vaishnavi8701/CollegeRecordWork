999589cfdd0becde401123c4bd4dd32d79e2ce4c
Nilesh D <nileshlund@gmail.com>
2018-10-02 18:36:36 +0530

    Changed int to void

--- EX2a.c
+++ EX2a.c
@@ -1,9 +1,8 @@
 # include <stdio.h>
-int main()
+void main()
 {
     int RollNo;
     printf("Enter Your Roll Number...");
     scanf("%d", &RollNo);
     printf("Hello, Your Roll Number Is...%d", RollNo);
-    return 0;
 }
