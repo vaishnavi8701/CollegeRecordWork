9f885cfb502e8685999b6da1eb69fc16e5375471
Nilesh2000 <nileshlund@gmail.com>
2018-10-03 17:36:47 +0530

    Added Relevant printf statements

--- EX2b.c
+++ EX2b.c
@@ -3,7 +3,7 @@
 void main()
 {
     int i=5, j=25, k=125;
-    printf("\n%3d\n %3d\n %3d\n", i, j, k);
-    printf("%5d\n %5d\n", i+j, i-k);
-    printf("%9.2f\n %7d\n", sqrt(i+j), abs(i-k));
+    printf("\nThe Value Of i Is...%3d\nThe Value Of j Is...%3d\nThe Value Of k Is...%3d\n", i, j, k);
+    printf("\nThe Value Of i+j Is...%5d\nThe Value Of i-k Is...%5d\n", i+j, i-k);
+    printf("\nThe Value Of sqrt(i+j) Is...%9.2f\nThe Value Of abs(i-k) Is...%7d\n", sqrt(i+j), abs(i-k));
 }
