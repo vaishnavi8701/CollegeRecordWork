85265fae8b4d1a011b3b3202fe2ff79a3613bd8e
Nilesh2000 <nileshlund@gmail.com>
2018-10-09 14:28:30 +0530

    Solved 3g

---
+++ EX3g.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+void main()
+{
+  int Sum=0, Count=0;
+   for(int i=1;i<=200;i++)
+    {
+     if(i%2==0)
+      {
+        Sum+=i;
+        Count++;
+      }
+    }
+ printf("\nThe Count Of Even Numbers From 1 To 200 Is...%d", Count);
+ printf("\nThe Sum Of The Serie Is...%d", Sum);      
+}
