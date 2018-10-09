8127366aebf53c4afe7c11a772ffcaa01f6991ba
Nilesh2000 <nileshlund@gmail.com>
2018-10-09 12:17:47 +0530

    Solved 3d

---
+++ EX3d.c
@@ -0,0 +1,26 @@
+# include <stdio.h>
+void main()
+{
+ int Sum=0;
+ printf("\n");
+ printf("\nThe Sum Of The Series Is...%d\n", Sum);
+}
+/*
+int i=2;
+while(i<=100)
+ {
+   Sum+=i;
+   i+=3;
+ }
+
+int i=2;
+ do
+  {
+   Sum+=i;
+   i+=3;
+ }while(i<=100);
+
+int i=2;
+for(int i=2;i<=100;i+=3)
+  Sum+=i;
+*/
