47eb4c6e51b2062825bec52aa0b78e3c4401f086
Nilesh D <nileshlund@gmail.com>
2018-10-10 19:05:46 +0530

    Segregated It Into Three Programs

--- EX3d.c
+++ EX3d.c
@@ -3,24 +3,35 @@ void main()
 {
  int Sum=0;
  printf("\n");
+ int i=2;
+  while(i<=100)
+   {
+    Sum+=i;
+    i+=3;
+   }
  printf("\nThe Sum Of The Series Is...%d\n", Sum);
 }
-/*
-int i=2;
-while(i<=100)
- {
-   Sum+=i;
-   i+=3;
- }
 
-int i=2;
- do
-  {
-   Sum+=i;
-   i+=3;
- }while(i<=100);
+# include <stdio.h>
+void main()
+{
+ int Sum=0;
+ printf("\n");
+ int i=2;
+  do
+   {
+    Sum+=i;
+    i+=3;
+   }while(i<=100);
+ printf("\nThe Sum Of The Series Is...%d\n", Sum);
+}
 
-int i=2;
-for(int i=2;i<=100;i+=3)
-  Sum+=i;
-*/
+# include <stdio.h>
+void main()
+{
+ int Sum=0, i;
+ printf("\n");
+  for(int i=2;i<=100;i+=3)
+   Sum+=i;
+ printf("\nThe Sum Of The Series Is...%d\n", Sum);
+}
