e3b2bbb3814cd909bb67bad75e5e46ddab2a48e5
Nilesh D <nileshlund@gmail.com>
2018-10-10 19:09:38 +0530

    Made Local Variables Global.

--- EX3b.c
+++ EX3b.c
@@ -1,16 +1,15 @@
 # include <stdio.h>
 int main()
 {
- int Days, Start_Day;
+ int i, k, Days, Start_Day;
  printf("\nEnter The Number Of Days In The Month...");
  scanf("%d", &Days);
  printf("Enter The Starting Day Of The Month (Sun=1 Sat=7)....");
  scanf("%d", &Start_Day);
  printf("\n  S  M  T  W  T  F  S\n");
- int k;
  for(k=0;k<Start_Day-1;k++)
  printf("   ");
-  for(int i=1;i<=Days;i++)
+  for(i=1;i<=Days;i++)
    {
     printf("%3d", i);
      if(++k>6)
