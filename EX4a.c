1caf73d644e788c2c4df9a1198ec79365fc112d1
Nilesh2000 <nileshlund@gmail.com>
2018-10-10 20:00:31 +0530

    Solved 4a, 4b

---
+++ EX4a.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+void main()
+{
+ int Arr[10]={1,2,3,4,5,6,7,8};
+  for(int i=0;i<7;i++)
+   {
+     if(Arr[i]==3)
+      printf("\nThe Element %d Exists In The Array.\n", Arr[i]);
+   }
+}
---
+++ EX4b.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+void main()
+{
+  int Size, Sum=0;
+  printf("\nEnter The Size Of The Array...");
+  scanf("%d", &Size);
+  int Arr[Size];
+   for(int i=0;i<Size;i++)
+    {
+     printf("Enter The Value Of Element %d:", i+1);
+     scanf("%d", &Arr[i]);
+     Sum+=Arr[i];
+    }
+ printf("\nThe Sum Of The Elements Of The Given Array Is...%d\n", Sum);
+}
