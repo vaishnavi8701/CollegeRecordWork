bf67191cd7bc49c381b20977e108340ca3e72d89
Nilesh2000 <nileshlund@gmail.com>
2018-10-14 07:41:26 +0530

    Changed Number Of Rows To 5

--- EX3h.c
+++ EX3h.c
@@ -3,9 +3,9 @@ int main()
 {
  int i, j;
  printf("\n");
-  for(i=1;i<=4;i++)
+  for(i=1;i<=5;i++)
    {
-    for(j=i;j<4;j++)
+    for(j=i;j<5;j++)
      printf(" ");
     for(j=1;j<=(2*i-1);j++)
      {
@@ -16,6 +16,6 @@ int main()
      }
     printf("\n");
    }
-   for(i=1;i<=4;i++)
+   for(i=1;i<=5;i++)
    printf("* ");
 }
