913ad9e40a9f4bc1bdd0f138073d7d7897b4e89b
Nilesh D <nileshlund@gmail.com>
2018-10-23 14:41:00 +0530

    Made the local declaration global and added spaces between array elements

--- EX4a.c
+++ EX4a.c
@@ -1,8 +1,8 @@
 # include <stdio.h>
 void main()
 {
- int Arr[10]={1,2,3,4,5,6,7,8};
-  for(int i=0;i<7;i++)
+ int i, Arr[10]={1, 2, 3, 4, 5, 6, 7, 8};
+  for(i=0;i<7;i++)
    {
      if(Arr[i]==3)
       printf("\nThe Element %d Exists In The Array.\n", Arr[i]);
