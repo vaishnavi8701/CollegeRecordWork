f9e403dbb412262fa575b67e4d5b671b16120c95
Nilesh2000 <nileshlund@gmail.com>
2018-10-24 04:34:51 +0530

    Made Two Declarations In One Line Instead Of Tw

--- EX3d.c
+++ EX3d.c
@@ -1,9 +1,8 @@
 # include <stdio.h>
 void main()
 {
- int Sum=0;
+ int Sum=0, i=2;
  printf("\n");
- int i=2;
   while(i<=100)
    {
     Sum+=i;
@@ -15,9 +14,8 @@ void main()
 # include <stdio.h>
 void main()
 {
- int Sum=0;
+ int Sum=0, i=2;
  printf("\n");
- int i=2;
   do
    {
     Sum+=i;
@@ -31,7 +29,7 @@ void main()
 {
  int Sum=0, i;
  printf("\n");
-  for(int i=2;i<=100;i+=3)
+  for(i=2;i<=100;i+=3)
    Sum+=i;
  printf("\nThe Sum Of The Series Is...%d\n", Sum);
 }
