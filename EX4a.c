42369619464de0130825f0ab0a9c0e85ba9e378d
Nilesh D <nileshlund@gmail.com>
2018-10-24 13:37:54 +0530

    Update EX4a.c

--- EX4a.c
+++ EX4a.c
@@ -1,10 +1,16 @@
 # include <stdio.h>
 void main()
 {
- int i, Arr[10]={1, 2, 3, 4, 5, 6, 7, 8};
+ int Flag=0, i, Arr[10]={1, 2, 3, 4, 5, 6, 7, 8};
   for(i=0;i<7;i++)
    {
      if(Arr[i]==3)
-      printf("\nThe Element %d Exists In The Array.\n", Arr[i]);
+      {
+       Flag=1;
+       printf("The Element %d Exists In The Array.\n", Arr[i]);
+      }
    }
+  if(Flag==0)
+   printf("The Element 3 Does Not Exist In The Array.\n");
 }
+
