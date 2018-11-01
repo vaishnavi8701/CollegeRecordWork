e47f6a931246065ce708ba3bc5198951e751138f
Nilesh2000 <nileshlund@gmail.com>
2018-11-01 08:57:34 +0530

    Optimised program by removing if statements

--- EX4d.c
+++ EX4d.c
@@ -12,26 +12,10 @@ void main()
         if(Arr[i]<0 || Arr[i]>100)
          printf("Marks Should Be In The Range Of 0-100 Only.\n");
       }while(Arr[i]<0 || Arr[i]>100);
-     if(Arr[i]>=0 && Arr[i]<=10)
-      Group[0]++;
-     else if(Arr[i]>=11 && Arr[i]<=20)
-      Group[1]++;
-     else if(Arr[i]>=21 && Arr[i]<=30)
-      Group[2]++;
-     else if(Arr[i]>=31 && Arr[i]<=40)
-      Group[3]++;
-     else if(Arr[i]>=41 && Arr[i]<=50)
-      Group[4]++;
-     else if(Arr[i]>=51 && Arr[i]<=60)
-      Group[5]++;
-     else if(Arr[i]>=61 && Arr[i]<=70)
-      Group[6]++;
-     else if(Arr[i]>=71 && Arr[i]<=80)
-      Group[7]++;
-     else if(Arr[i]>=81 && Arr[i]<=90)
-      Group[8]++;
-     else if(Arr[i]>=91 && Arr[i]<=100)
-      Group[9]++;
+    if(Arr[i]%10==0)
+     Arr[i]--;
+     Arr[i]/=10;
+     Group[Arr[i]]++;
    }
   for(i=0, j=0, k=10; i<10; i++, j+=10, k+=10)
    {
