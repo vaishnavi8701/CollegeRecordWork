eb695dc93f4189bcdd057db6e05e70d12b492346
Nilesh D <nileshlund@gmail.com>
2018-11-28 19:59:58 +0530

    Solved 7b

---
+++ EX7b.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+int computeFunction(int a, int b);
+void main()
+{
+ int x, y, Res;
+ printf("\nEnter The Values Of x and y...");
+ scanf("%d %d", &x, &y);
+ Res=computeFunction(x, y);
+ printf("\nThe Result Of f(%d, %d) Is...%d\n",x, y, Res);
+}
+int computeFunction(int a, int b)
+{
+ if(b<=a)
+  return computeFunction(a-b, b)+1;
+ else if(a<b)
+  return 0;
+}
