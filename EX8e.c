f5f1d87785d9c40fa13113b4d468d3d5d1b57395
Nilesh2000 <nileshlund@gmail.com>
2018-12-02 11:10:40 +0530

    Wrote both the structures Seems to be an error in declaration of DOJ.

---
+++ EX8e.c
@@ -0,0 +1,16 @@
+# include <stdio.h>
+struct Date
+{
+  int Day, Month, Year;
+};
+struct Employee
+{
+  int Code, deptNo;
+  char Name[20];
+  float Salary;
+  Date DOJ;
+};
+void main()
+{
+   
+}
