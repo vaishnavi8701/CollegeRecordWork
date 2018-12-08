76a49bcb4562b0eca3b45e846b3d55029a86c43a
Nilesh2000 <nileshlund@gmail.com>
2018-12-08 21:58:38 +0530

    Solved 10b (Also pushed read and write files)

---
+++ EX10b.c
@@ -0,0 +1,25 @@
+# include <stdio.h>
+# include <stdlib.h>
+void main()
+{
+ char fileName1[10], fileName2[10], Str[100];
+ printf("\nEnter The First Filename...");
+ scanf("%s", fileName1);
+ printf("Enter The Second Filename...");
+ scanf("%s", fileName2);
+ FILE *fptr1, *fptr2;
+ fptr1=fopen(fileName1, "r");
+  if(fptr1==NULL)
+   {
+     printf("\nFile Does Not Exist.");
+     exit(0);
+   }
+ fptr2=fopen(fileName2, "w");
+  while(!(feof(fptr1)))
+   {
+    fgets(Str, 100, fptr1);
+    fputs(Str, fptr2);
+   }
+ fclose(fptr1);
+ fclose(fptr2);
+}
