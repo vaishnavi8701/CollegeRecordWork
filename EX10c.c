8a9b546a58a86b7bfbc2aba96d4147d0f3f42bdc
Nilesh2000 <nileshlund@gmail.com>
2018-12-09 11:37:52 +0530

    program doesn't seem to be working.

--- EX10c.c
+++ EX10c.c
@@ -1,17 +1,26 @@
 # include <stdio.h>
+# include <stdlib.h>
 void main()
 {
  FILE *fptr1;
  fptr1=fopen("readFile.txt", "r");
+  if(fptr1==NULL)
+   {
+    printf("\nFile Does Not Exist.");
+    exit(0);
+   }
  int Lines=0, Count=0;
  char Ch;
   while(!(feof(fptr1)))
    {
-     Ch=fgetc(fptr1);
-      if(Ch=='\n')
-       Lines++;
-     Count++;
+    Ch=fgetc(fptr1);
+     if(feof(fptr1))
+      break;
+     if(Ch=='\n')
+      Lines++;
+    Count++;
    }
+ fclose(fptr1);
  printf("\nThe Number Of Characters In The File Is...%d", Count);
  printf("\nThe Number Of Lines In The File Is...%d\n", Lines);
 }
