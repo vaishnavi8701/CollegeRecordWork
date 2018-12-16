b62a71f6c609d806956b60f12c2461445c7da4d4
Nilesh2000 <nileshlund@gmail.com>
2018-12-17 05:19:33 +0530

    Done and dusted with this program.

--- EX8a.c
+++ EX8a.c
@@ -5,39 +5,47 @@ struct Ticket
  int seatNo, custAge;
  float ticketCost;
 };
+int readDetails(struct Ticket []);
 void printTicket(struct Ticket [], int);
 void main()
 {
- struct Ticket Var[100];
- int ticketNo=0;
- char Ch='y';
- printf("---------Boat Ticket Booking---------");
-  do
-   {
-    printf("\nEnter The Customer Name...");
-    scanf("%s", Var[ticketNo].custName);
-    printf("Enter The Customer Age...");
-    scanf("%d", &Var[ticketNo].custAge);
-    printf("Enter The Boarding Time...");
-    scanf("%s", Var[ticketNo].boardTime);
-    printf("Enter The Seat Number...");
-    scanf("%d", &Var[ticketNo].seatNo);
-    printf("Enter The Ticket Cost...");
-    scanf("%f", &Var[ticketNo].ticketCost);
-    ticketNo++;
-    printf("\nWant To Book Another Ticket<y/n>...");
-    scanf("%c", &Ch);
-  }while(Ch=='y' && ticketNo<=100);
- printf("\nBooked ticket details");
- //printTicket(Var, ticketNo);
-}/*
-void printTicket(struct Ticket t[], int n)
+ struct Ticket Var[10];
+ int ticketNo=readDetails(Var);
+ printTicket(Var, ticketNo);
+}
+int readDetails(struct Ticket Obj[])
+{
+  printf("\n---------Boat Ticket Booking---------\n");
+  int n=0;
+  char Ch='y';
+   do
+    {
+     printf("\nEnter The Customer Name...");
+     scanf("%s", Obj[n].custName);
+     printf("Enter The Customer Age...");
+     scanf("%d", &Obj[n].custAge);
+     printf("Enter The Boarding Time...");
+     scanf("%s", Obj[n].boardTime);
+     printf("Enter The Seat Number...");
+     scanf("%d", &Obj[n].seatNo);
+     printf("Enter The Ticket Cost...");
+     scanf("%f", &Obj[n].ticketCost);
+     n++;
+     printf("\nWant To Book Another Ticket<y/n>...");
+     scanf(" %c", &Ch);
+   }while(Ch=='y' && n<=10);
+ return n;
+}
+void printTicket(struct Ticket Obj[], int n)
 {
  int i;
+ printf("\nBooked Ticket Details:-\n");
  for(i=0;i<n;i++)
   {
-   printf("\n	");
-   printf("\nName: %s\nAge: %d\nTime: %s\nSeat.No: %d\nCost: %f",t[i].name,t[i].age,t[i].tim e,t[i].seatno,t[i].cost);
-printf("\n	");
+   printf("\nCustomer Name :%s", Obj[i].custName);
+   printf("\nCustomer Age :%d", Obj[i].custAge);
+   printf("\nBoarding Time :%s", Obj[i].boardTime);
+   printf("\nSeat Number :%d", Obj[i].seatNo);
+   printf("\nTicket Cost :%.2f\n", Obj[i].ticketCost);
+  }
 }
-}*/
