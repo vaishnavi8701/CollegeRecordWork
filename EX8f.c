16a2f102ba76ed33635342238d841f5c6d75fd2f
Nilesh2000 <nileshlund@gmail.com>
2018-12-16 15:53:42 +0530

    maxSalary not working. Sort function is a mess. My mind is tired.

--- EX8f.c
+++ EX8f.c
@@ -15,7 +15,8 @@ struct Employee
 void readDetails(struct Employee *, int);
 void dispDetails(struct Employee *, int);
 int minSalary(struct Employee*, int);
-//void maxSalary();
+int maxSalary(struct Employee*, int);
+void sortSalary(struct Employee* [], int);
 void Menu();
 void main()
 {
@@ -26,25 +27,25 @@ void readDetails(struct Employee *Ptr, int Size)
   int i;
    for(i=0;i<Size;i++)
     {
-      printf("\nEnter Employee Code...");
+      /*printf("\nEnter Employee Code...");
       scanf("%d", &(Ptr+i)->empCode);
       printf("Enter Employee Name...");
-      scanf("%s", &(Ptr+i)->empName);
+      scanf("%s", &(Ptr+i)->empName);*/
       printf("Enter Employee Salary...");
       scanf("%f", &(Ptr+i)->empSalary);
-      printf("Enter Employee Department Number...");
+      /*printf("Enter Employee Department Number...");
       scanf("%d", &(Ptr+i)->deptNo);
       printf("Enter Day, Month And Year Of Joining...");
-      scanf("%d %d %d", &(Ptr+i)->DOJ.Day, &(Ptr+i)->DOJ.Month, &(Ptr+i)->DOJ.Year);
+      scanf("%d %d %d", &(Ptr+i)->DOJ.Day, &(Ptr+i)->DOJ.Month, &(Ptr+i)->DOJ.Year);*/
     }
 }
 void dispDetails(struct Employee *Ptr, int Pos)
 {
- printf("Employee Code...%d", (Ptr+Pos)->empCode);
- printf("\nEmployee Name...%s", (Ptr+Pos)->empName);
+ /*printf("Employee Code...%d", (Ptr+Pos)->empCode);
+ printf("\nEmployee Name...%s", (Ptr+Pos)->empName);*/
  printf("\nEmployee Salary...%.2f", (Ptr+Pos)->empSalary);
- printf("\nEmployee Department Number...%d", (Ptr+Pos)->deptNo);
- printf("\nEmployee Date Of Joining...%d/%d/%d\n", (Ptr+Pos)->DOJ.Day, (Ptr+Pos)->DOJ.Month, (Ptr+Pos)->DOJ.Year);
+ /*printf("\nEmployee Department Number...%d", (Ptr+Pos)->deptNo);
+ printf("\nEmployee Date Of Joining...%d/%d/%d\n", (Ptr+Pos)->DOJ.Day, (Ptr+Pos)->DOJ.Month, (Ptr+Pos)->DOJ.Year);*/
 }
 int minSalary(struct Employee *Ptr, int Size)
 {
@@ -54,12 +55,40 @@ int minSalary(struct Employee *Ptr, int Size)
     if(Ptr->empSalary>(Ptr+i)->empSalary)
      Pos=i;
    }
- printf("\nEmployee With Lowest Salary:-");
+ printf("\nEmployee With Lowest Salary:-\n");
  return Pos;
+}
+int maxSalary(struct Employee *Ptr, int Size)
+{
+ int i, Pos=0;
+  for(i=1;i<Size;i++)
+   {
+    if(Ptr->empSalary<(Ptr+i)->empSalary)
+     Pos=i;
+   }
+ printf("\nEmployee With Highest Salary:-\n");
+ return Pos;
+}
+void sortSalary(struct Employee *Ptr[], int Size)
+{
+  int i, j;
+  struct Employee *Temp=NULL;
+  for(i=0;i<Size-1;i++)
+   {
+    for(j=0;j<Size-i-1;j++)
+     {
+      if(Ptr[j]->empSalary>Ptr[j+1]->empSalary)
+       {
+        Temp=Ptr[j];
+        Ptr[j]=Ptr[j+1];
+        Ptr[j+1]=Temp;
+       }
+     }
+   }
 }
 void Menu()
 {
-  int Choice, n, Position;
+  int i, Choice, n, minPos, maxPos;
   struct Employee *Var;
    while(Choice!=6)
     {
@@ -85,11 +114,15 @@ void Menu()
                  }
                readDetails(Var, n);
                 break;
-        case 2:Position=minSalary(Var, n);
-               dispDetails(Var, Position);
-                break;/*
-        case 3:dispSalary();
+        case 2:minPos=minSalary(Var, n);
+               dispDetails(Var, minPos);
+               maxPos=maxSalary(Var, n);
+               dispDetails(Var, maxPos);
                 break;
+        case 3:sortSalary(Var, n);
+                for(i=0;i<n;i++)
+                 dispDetails(Var, n);
+                break;/*
         case 4:dispDOJ();
                 break;
         case 5:dispDept();
