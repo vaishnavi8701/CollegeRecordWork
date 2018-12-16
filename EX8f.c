5ae7c897919979d2160316ea48c9d1ebed735e47
Nilesh2000 <nileshlund@gmail.com>
2018-12-16 12:28:23 +0530

    minSalary function now works. Still a long way to go.

--- EX8f.c
+++ EX8f.c
@@ -1,5 +1,6 @@
 # include <stdio.h>
 # include <stdlib.h>
+# include <conio.h>
 struct Date
 {
   int Day, Month, Year;
@@ -11,20 +12,19 @@ struct Employee
   float empSalary;
   struct Date DOJ;
 };
+void readDetails(struct Employee *, int);
+void dispDetails(struct Employee *, int);
+int minSalary(struct Employee*, int);
+//void maxSalary();
 void Menu();
-void readDetails();
 void main()
 {
   Menu();
 }
-void readDetails()
+void readDetails(struct Employee *Ptr, int Size)
 {
-  struct Employee *Ptr;
-  int n, i;
-  printf("\nEnter The Number Of Employees...");
-  scanf("%d", &n);
-  Ptr=(struct Employee*)malloc(n*sizeof(struct Employee));
-   for(i=0;i<n;i++)
+  int i;
+   for(i=0;i<Size;i++)
     {
       printf("\nEnter Employee Code...");
       scanf("%d", &(Ptr+i)->empCode);
@@ -38,9 +38,29 @@ void readDetails()
       scanf("%d %d %d", &(Ptr+i)->DOJ.Day, &(Ptr+i)->DOJ.Month, &(Ptr+i)->DOJ.Year);
     }
 }
+void dispDetails(struct Employee *Ptr, int Pos)
+{
+ printf("Employee Code...%d", (Ptr+Pos)->empCode);
+ printf("\nEmployee Name...%s", (Ptr+Pos)->empName);
+ printf("\nEmployee Salary...%.2f", (Ptr+Pos)->empSalary);
+ printf("\nEmployee Department Number...%d", (Ptr+Pos)->deptNo);
+ printf("\nEmployee Date Of Joining...%d/%d/%d\n", (Ptr+Pos)->DOJ.Day, (Ptr+Pos)->DOJ.Month, (Ptr+Pos)->DOJ.Year);
+}
+int minSalary(struct Employee *Ptr, int Size)
+{
+ int i, Pos=0;
+  for(i=1;i<Size;i++)
+   {
+    if(Ptr->empSalary>(Ptr+i)->empSalary)
+     Pos=i;
+   }
+ printf("\nEmployee With Lowest Salary:-");
+ return Pos;
+}
 void Menu()
 {
-  int Choice;
+  int Choice, n, Position;
+  struct Employee *Var;
    while(Choice!=6)
     {
      printf("\nEnter Any One Of The Following Choices:-");
@@ -54,8 +74,30 @@ void Menu()
      scanf("%d", &Choice);
       switch(Choice)
        {
-         case 1:readDetails();
+        case 1:printf("\nEnter The Number Of Employees...");
+               scanf("%d", &n);
+               Var=(struct Employee*)malloc(n*sizeof(struct Employee));
+                if(Var==NULL)
+                 {
+                  printf("\nMemory Not Allotted.");
+                  getch();
+                  exit(0);
+                 }
+               readDetails(Var, n);
+                break;
+        case 2:Position=minSalary(Var, n);
+               dispDetails(Var, Position);
+                break;/*
+        case 3:dispSalary();
+                break;
+        case 4:dispDOJ();
+                break;
+        case 5:dispDept();
+                break;*/
+        case 6:printf("\nThank You For Using The Program.");
+                break;
+        default:printf("\nInvalid Choice.\n");
                  break;
        }
-    }   
+    }
 }
