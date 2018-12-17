68c351f8fd37182ab95b212c4f0213faab6f5109
Nilesh2000 <nileshlund@gmail.com>
2018-12-17 22:14:30 +0530

    Edited the program according to the question.

--- EX9a.c
+++ EX9a.c
@@ -1,12 +1,9 @@
 # include <stdio.h>
 void main()
 {
-  int a=2, b=3;
-  int *p, *q;
-  p=&a;
-  q=&b;
-  printf("\nAddress Of a...%p", p);
-  printf("\nAddress Of b...%p", q);
-  printf("\nValue At p...%d", *p);
-  printf("\nValue At q...%d\n", *q);
+ int a, *p;
+ p=&a;
+ *p=10;
+ printf("\nAddress Of a...%p", p);
+ printf("\nValue At p...%d\n", *p);
 }
