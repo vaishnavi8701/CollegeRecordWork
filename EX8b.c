aac541a99cd2a96ffa8d337fa63cb0cb364a6a0c
Nilesh2000 <nileshlund@gmail.com>
2018-12-17 05:31:54 +0530

    This Code Is MESSED UP.

--- EX8b.c
+++ EX8b.c
@@ -1,43 +1,79 @@
 # include <stdio.h>
 struct Ticket
 {
-  int pnrNo, trainNo, seatNo, noOfPassengers;
-  char custName[10], trainName[20], DOJ[15], coachNo[3], arrTime[6], deptTime[6];
+char name[20], date[20], source[20], dist[20], tname[20];
+int age,seatno,clas,id;
+float cost;
+char status;
 };
-void main()
+int search(struct Ticket Obj[], int n, int id);
+void status(struct ticket Obj[], int n);
+int main()
 {
- struct Ticket t;
- printf("\nEnter Ticket Details As Folows:-\n");
- printf("Enter PNR Number...");
- scanf("%d", &t.pnrNo);
- printf("Enter Customer Name...");
- scanf("%s", t.custName);
- printf("Enter Train Number...");
- scanf("%d", &t.trainNo);
- printf("Enter Train Name...");
- getchar();
- gets(t.trainName);
- printf("Enter Date Of Journey...");
- scanf("%s", t.DOJ);
- printf("Enter Coach Number...");
- scanf("%s", t.coachNo);
- printf("Enter Seat Number...");
- scanf("%d", &t.seatNo);
- printf("Enter Number Of Passengers...");
- scanf("%d", &t.noOfPassengers);
- printf("Enter Departure Time...");
- scanf("%s", t.deptTime);
- printf("Enter Arrival Time...");
- scanf("%s", t.arrTime);
- printf("\nTicket:-\n");
- printf("\nPNR Number :%d", t.pnrNo);
- printf("\nCustomer Name :%s", t.custName);
- printf("\n__________________________________________________________________");
- printf("\nTrain Number     Train Name       Date Of Journey   Coach    Seat");
- printf("\n__________________________________________________________________");
- printf("\n%d \t\t %s   %s \t    %s %8d", t.trainNo, t.trainName, t.DOJ, t.coachNo, t.seatNo);
- printf("\n__________________________________________________________________");
- printf("\nNumber Of Passengers     Arrival Time    Departure Time");
- printf("\n__________________________________________________________________");
- printf("\n%d \t\t\t  %s \t %s\n", t.noOfPassengers, t.deptTime, t.arrTime);
+ struct ticket Var[10];
+ char Ch='y';
+ int i=0, Choice, id;
+ printf("----Welcome To Southern Railways----");
+  while(Choice!=4)
+   {
+    printf("\nEnter Any One Of The Following Choices:-");
+    printf("\n1. Book A Ticket");
+    printf("\n2. Search Your Ticket");
+    printf("\n3. View Ticket Status");
+    printf("\n4. Terminate The Program");
+    printf("\nYour Choice Is...");
+    scanf("%d", &Choice);
+     switch(Choice)
+      {
+       case 1:do
+               {
+                printf("\nEnter the name,date,source,distination,train name,age,seatno,class,id,cost,status:");
+                scanf("%s%s%s%s%s%d%d%d%d%f %c",t[i].name,t[i].date,t[i].source,t[i].dist,t[i].tname, &t[i].age,&t[i].seatno,&t[i].clas,&t[i].id,&t[i].cost,&t[i].status);
+                i++;
+                printf("\nDo you want to book another ticket?(y/n)"); scanf(" %c",&ch);
+}             while(ch == 'y' && i<=100);
+}break;
+case 2:
+{
+printf("\n----Search Passenger	");
+printf("\nEnter the passenger id to search:"); scanf("%d",&id);
+if(search(t,i,id))
+printf("\nFound");
+else
+printf("\nNot found...");
+case 3:
+printf("\n	");
+}break;
+{
+printf("\n----Status Check	");
+status(t,i);
+printf("\n	");
+}break;
+case 4:
+{
+}break;
+default:printf("\nInvalid option...!");
+}
+}while(mch != 4);
+}
+int search(struct ticket t[],int n,int id)
+{
+int i; for(i=0;i<n;i++)
+{
+if(t[i].id == id)
+{
+printf("\n	");
+printf("\nName: %s\nDate: %s\nSource: %s\nDestination: %s\nTrain name: %s\nAge: %d\nSeat.no: %d\nClass: %d\nId: %d\nCost: %f\nStatus: %c",t[i].name,t[i].date,t[ i].source,t[i].dist,t[i].tname,t[i].age,t[i].seatno,t[i].clas,t[i].id,t[i].cost,t[i].status);
+printf("\n	");
+return 1;
+}
+}
+return 0;
+}
+void status(struct ticket t[], int n)
+{
+int i;
+printf("\nName\t\tStatus");
+for(i=0;i<n;i++)
+printf("\n%s\t\t%c",t[i].name,t[i].status);
 }
