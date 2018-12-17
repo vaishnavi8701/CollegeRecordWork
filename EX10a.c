2631052d30702a8c6d50aef895705c9448d3a837
Nilesh2000 <nileshlund@gmail.com>
2018-12-17 22:08:08 +0530

    Edited Program To Use Only 1 file, i.e for writing. Accepted input.

--- EX10a.c
+++ EX10a.c
@@ -2,22 +2,18 @@
 # include <stdlib.h>
 void main()
 {
- FILE *fptr1, *fptr2;
- char Ch;
- fptr1=fopen("readFile.txt", "r");
-  if(fptr1==NULL)
-   {
-    printf("\nFile Does Not Exist.");
-    exit(0);
-   }
- fptr2=fopen("writeFile1.txt", "w");
-  while(!(feof(fptr1)))
-   {
-    Ch=fgetc(fptr1);
-     if(feof(fptr1))
-      break;
-    fputc(Ch, fptr2);
-   }
- fclose(fptr1);
- fclose(fptr2);
+  FILE *fptr;
+  char Ch;
+  fptr=fopen("writeFile1.txt", "w");
+   if(fptr==NULL)
+    {
+      printf("\nFile Does Not Exist.");
+      exit(0);
+    }
+   do
+    {
+     Ch=getchar();
+     fputc(Ch, fptr);
+   }while(Ch!='\n');
+  fclose(fptr);
 }
