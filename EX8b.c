b3ed0ff0e2f0ea490f2c6a04a7e9b4272e990516
Nilesh2000 <nileshlund@gmail.com>
2018-12-18 04:37:24 +0530

    I guess I have solved it. (Change variable name if you get time.)

--- EX8b.c
+++ EX8b.c
@@ -1,99 +1,110 @@
 # include <stdio.h>
 struct Ticket
 {
-char name[20], date[20], source[20], dist[20], tname[20];
-int age,seatno,class,id;
-float cost;
-char status;
+ int ticketId;
+ char custName[20], DOD[20], startPoint[20], destPoint[20], trainName[20];
+ int custAge, seatNo;
+ char custClass[5];
+ float ticketCost;
+ char Status;
 };
-int search(struct Ticket Obj[], int n, int id);
-void status(struct ticket Obj[], int n);
-int main()
+int readTicket(struct Ticket []);
+int searchTicket(struct Ticket [], int, int);
+void viewStatus(struct Ticket [], int);
+void main()
 {
- struct ticket Var[10];
+ struct Ticket Var[10];
  char Ch='y';
- int i=0, Choice, id;
+ int Choice, ticketNos, Id;
  printf("----Welcome To Southern Railways----");
   while(Choice!=4)
    {
     printf("\nEnter Any One Of The Following Choices:-");
     printf("\n1. Book A Ticket");
-    printf("\n2. Search Your Ticket");
+    printf("\n2. Search For Your Ticket");
     printf("\n3. View Ticket Status");
     printf("\n4. Terminate The Program");
     printf("\nYour Choice Is...");
     scanf("%d", &Choice);
      switch(Choice)
       {
-       case 1:do
-               {
-                printf("\nEnter Ticket Id...");
-                scanf("%d", &Obj.ticketId);
-                printf("Enter The Customer Name...");
-                scanf("%d", Obj[i].custName);
-                printf("Enter The Departure Date...");
-                scanf("%s", Obj[i].DOD);
-                printf("Enter The Starting Point...");
-                scanf("%s", Obj[i].startPoint);
-                printf("Enter The Destination...");
-                printf("%s", Obj[i].destPoint);
-                printf("Enter The Train Name...");
-                scanf("%d", &Obj[i].trainName);
-                printf("Enter Customer Age...");
-                scanf("%d", &Obj[i].custAge);
-                printf("Enter The Seat Number...");
-                scanf("%d", &Obj[i].seatNo);
-                printf("Enter Customer Class...");
-                scanf("%s", &Obj[i].custClass)
-                printf("Enter Ticket Cost...");
-                scanf("%f", &Obj[i].ticketCost);
-                printf("Enter Ticket Status<y/n>...");
-                scanf("%c", &Obj[i].Status);
-                i++;
-                printf("\nDo you want to book another ticket?(y/n)"); scanf(" %c",&ch);
-}             while(ch == 'y' && i<=100);
-}break;
-case 2:
-{
-printf("\n----Search Passenger	");
-printf("\nEnter the passenger id to search:"); scanf("%d",&id);
-if(search(t,i,id))
-printf("\nFound");
-else
-printf("\nNot found...");
-case 3:
-printf("\n	");
-}break;
-{
-printf("\n----Status Check	");
-status(t,i);
-printf("\n	");
-}break;
-case 4:
-{
-}break;
-default:printf("\nInvalid option...!");
+       case 1:ticketNos=readTicket(Var);
+              break;
+       case 2:printf("\n----Search Passenger----");
+              printf("\nEnter The Ticket Id To Be Searched For...");
+              scanf("%d", &Id);
+               if(searchTicket(Var, ticketNos, Id)==0)
+                printf("No Such Ticket Found\n");
+               break;
+       case 3:viewStatus(Var, ticketNos);
+               break;
+       case 4:printf("\nThank You For Using The Program.\n");
+               break;
+       default:printf("\nInvalid Choice.\n");
+      }
+   }
 }
-}while(mch != 4);
-}
-int search(struct ticket t[],int n,int id)
+int readTicket(struct Ticket Obj[])
 {
-int i; for(i=0;i<n;i++)
-{
-if(t[i].id==id)
-{
-printf("\n	");
-printf("\nName: %s\nDate: %s\nSource: %s\nDestination: %s\nTrain name: %s\nAge: %d\nSeat.no: %d\nClass: %d\nId: %d\nCost: %f\nStatus: %c",t[i].name,t[i].date,t[ i].source,t[i].dist,t[i].tname,t[i].age,t[i].seatno,t[i].clas,t[i].id,t[i].cost,t[i].status);
-printf("\n	");
-return 1;
+ char Ch='y';
+ int i=0;
+  do
+   {
+    printf("\nEnter Ticket Id...");
+    scanf("%d", &Obj[i].ticketId);
+    printf("Enter The Passenger Name...");
+    scanf("%s", Obj[i].custName);
+    printf("Enter Passesnger Age...");
+    scanf("%d", &Obj[i].custAge);
+    printf("Enter The Departure Date...");
+    scanf("%s", Obj[i].DOD);
+    printf("Enter The Starting Point...");
+    scanf("%s", Obj[i].startPoint);
+    printf("Enter The Destination...");
+    scanf("%s", Obj[i].destPoint);
+    printf("Enter The Train Name...");
+    scanf("%s", Obj[i].trainName);
+    printf("Enter Passenger Class...");
+    scanf("%s", Obj[i].custClass);
+    printf("Enter The Seat Number...");
+    scanf("%d", &Obj[i].seatNo);
+    printf("Enter Ticket Cost...");
+    scanf("%f", &Obj[i].ticketCost);
+    printf("Enter Ticket Status<y/n>...");
+    scanf(" %c", &Obj[i].Status);
+    i++;
+    printf("\nDo You Want To Book Another Ticket<y/n>...");
+    scanf(" %c", &Ch);
+   }while(Ch=='y' && i<=100);
+ return i;
 }
-}
-return 0;
-}
-void status(struct ticket t[], int n)
+int searchTicket(struct Ticket Obj[], int n, int searchId)
 {
-int i;
-printf("\nName\t\tStatus");
-for(i=0;i<n;i++)
-printf("\n%s\t\t%c",t[i].name,t[i].status);
+ int i;
+  for(i=0;i<n;i++)
+   {
+    if(Obj[i].ticketId==searchId)
+     {
+      printf("\nPasseneger Name :%s", Obj[i].custName);
+      printf("\nPassenger Age :%d", Obj[i].custAge);
+      printf("\nDeparture Date :%s", Obj[i].DOD);
+      printf("\nStarting Point :%s", Obj[i].startPoint);
+      printf("\nDestination :%s", Obj[i].destPoint);
+      printf("\nTrain Name :%s", Obj[i].trainName);
+      printf("\nPassenger Class :%s", Obj[i].custClass);
+      printf("\nSeat Number :%d", Obj[i].seatNo);
+      printf("\nTicket Cost :%.2f\n", Obj[i].ticketCost);
+      return 1;
+     }
+   }
+ return 0;
+}
+void viewStatus(struct Ticket Obj[], int n)
+{
+ int i;
+ printf("\n----Status Check----");
+ printf("\nName\t\tStatus");
+  for(i=0;i<n;i++)
+   printf("\n%s\t\t%c", Obj[i].custName, Obj[i].Status);
+ printf("\n");
 }
