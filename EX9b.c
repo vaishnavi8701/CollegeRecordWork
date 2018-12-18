3a5110086a3945f0dfb150eb95c7acbed7ab32f5
Nilesh2000 <nileshlund@gmail.com>
2018-12-18 20:54:31 +0530

    Edited the program after Ma'am asked me to.

--- EX9b.c
+++ EX9b.c
@@ -1,24 +1,15 @@
 # include <stdio.h>
-struct Distance
+struct Example
 {
- int kmCount, mCount;
+  int x;
+  float y;
 };
 void main()
 {
- struct Distance d1, d2, d5={0,0};
- struct Distance *d3, *d4;
- d3=&d1;
- d4=&d2;
- printf("\nEnter Distance 1 In Kilometres And Metres...");
- scanf("%d %d", &d3->kmCount, &d3->mCount);
- printf("Enter Distance 2 In Kilomteres And Metres...");
- scanf("%d %d",&d4->kmCount,&d4->mCount);
- d5.kmCount=d3->kmCount+d4->kmCount;
- d5.mCount=d3->mCount+d4->mCount;
-  if(d5.mCount>=1000)
-   {
-    d5.mCount-=1000;
-    d5.kmCount++;;
-   }
- printf("\nThe Sum Of The Two Distances Is %d Kilometres And %d Metres\n", d5.kmCount, d5.mCount);
+  struct Example Var;
+  struct Example *Ptr;
+  Ptr=&Var;
+  Ptr->x=5;
+  Ptr->y=3.142;
+  printf("\nValue Stored In x and y Is %d and %.3f respectively.\n", Ptr->x, Ptr->y);
 }
