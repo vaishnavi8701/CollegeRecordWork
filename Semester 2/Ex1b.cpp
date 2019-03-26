d57f1df2940961d9e5462838a0662a54d6f91175
Nilesh2000 <nileshlund@gmail.com>
2019-03-26 21:56:10 +0530

    Enhanced the source code.

--- Semester 2/Ex1b.cpp
+++ Semester 2/Ex1b.cpp
@@ -4,58 +4,78 @@ using namespace std;
 
 class Travel
 {
-    int noOfAdults, noOfChildren;
-    float Distance, totalCost;
+    int tCode, noOfAdults, noOfChildren;
+    float Distance, totalFare;
+    string Src, Dest;
   public:
     Travel()
     {
-        noOfAdults = noOfChildren = Distance = totalCost = 0;
+        tCode = 1001;
+        Src = "Agra";
+        Dest = "Chennai";
+        Distance = 400.00;
+        noOfAdults = 3;
+        noOfChildren = 2;
     }
-    Travel(int Num1, int Num2, float Space, float Price)
+    Travel(const Travel &t)
     {
-        noOfAdults = Num1;
-        noOfChildren = Num2;
-        Distance = Space;
-        totalCost = Price;
-    }
-    Travel(Travel &t)
-    {
+        tCode = t.tCode;
+        Src = t.Src;
+        Dest = t.Src;
+        Distance = t.Distance;
         noOfAdults = t.noOfAdults;
         noOfChildren = t.noOfChildren;
-        Distance = t.Distance;
-        totalCost = t.totalCost;
     }
     void getDetails()
     {
-        cout << "\nEnter The Number Of Adults : ";
+        cout << "\nEnter The Travel Code : ";
+        cin >> tCode;
+        cout << "Enter The Source : ";
+        cin >> Src;
+        cout << "Enter The Destination : ";
+        cin >> Dest;
+        cout << "Enter The Travel Distance : ";
+        cin >> Distance;
+        cout << "Enter The Number Of Adults : ";
         cin >> noOfAdults;
         cout << "Enter The Number Of Children : ";
         cin >> noOfChildren;
-        cout << "Enter The Travel Distance : ";
-        cin >> Distance;
     }
     int computeCost()
     {
-         if(Distance >= 500)
+      float childFare, adultFare;
+        if(Distance < 500)
           {
-            int d = Distance - 500;
-            totalCost += d * ( (noOfAdults * 10) + (noOfChildren * 6) );
-            totalCost += 500 * ( (noOfAdults * 8) + (noOfChildren * 5) );
-            return 0;
+              adultFare = 200 * noOfAdults;
+              childFare = 100 * noOfChildren;
           }
-        totalCost = Distance * ( (noOfAdults * 8) + (noOfChildren * 5) );
+        else if(Distance > 500 && Distance < 1000)
+          {
+              adultFare = 300 * noOfAdults;
+              childFare = 150 * noOfChildren;
+          }
+        else
+          {
+            adultFare = 500 * noOfAdults;
+            childFare = 250 * noOfChildren;
+          }
+      totalFare = childFare + adultFare;
     }
     void displayDetails()
     {
-        cout << "\nNumber Of Adults : " << noOfAdults
+      cout   << "\nTravel Code : " << tCode
+             << "\nNumber Of Adults : " << noOfAdults
              << "\nNumber Of Children : " << noOfChildren
+             << "\nSource : " << Src
+             << "\nDestination : " << Dest
              << "\nTravel Distance : " << Distance << " Km"
-             << "\nTotal Cost : Rs. " << totalCost << "/-"
+             << "\nTotal Cost : Rs. " << totalFare << "/-"
              << "\n";
     }
 ~Travel()
      {
-        noOfAdults = noOfChildren = Distance = totalCost = 0;
+        tCode = noOfAdults = noOfChildren = Distance = totalFare = 0;
+        Src[0] = Dest[0] = '\0';
      }
 };
 
