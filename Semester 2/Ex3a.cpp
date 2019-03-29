de42e4d11296cf615cde0fbd4dbcde22453da6cd
Nilesh2000 <nileshlund@gmail.com>
2019-03-29 08:39:57 +0530

    Added a lot of new features.

--- Semester 2/Ex3a.cpp
+++ Semester 2/Ex3a.cpp
@@ -2,43 +2,182 @@
 
 using namespace std;
 
-class Array
+class Matrix
 {
+    int Arr[3][3];
   public:
 
-   void calcSum(int intArr[5])
-   {
-        int Sum = 0;
-        cout << "\nEnter 5 Array Elements : ";
-          for(int i = 0 ; i < 5 ; i++)
+    void readMatrix()
+    {
+          for(int i = 0 ; i < 3 ; i++)
            {
-              cin >> intArr[i];
-              Sum += intArr[i];
+              for(int j = 0 ; j < 3 ; j++)
+               {
+                  cin >> Arr[i][j];
+               }
            }
-        cout << "The Sum Of The Array Elements Is : " << Sum << endl;
-   }
+    }
 
-   void calcSum(float floatArr[5])
-   {
-        float Sum = 0;
-        cout << "\nEnter 5 Array Elements : ";
-          for(int i = 0 ; i < 5 ; i++)
+    void printMatrix()
+    {
+          for(int i = 0 ; i < 3 ; i++)
            {
-              cin >> floatArr[i];
-              Sum += floatArr[i];
+              for(int j = 0 ; j < 3 ; j++)
+               {
+                 cout << Arr[i][j] << " ";
+               }
+             cout << endl;
            }
-        cout << "The Sum Of The Array Elements Is : " << Sum << endl;
-   }
+    }
+
+    Matrix operator +(Matrix &Obj)
+    {
+      Matrix Res;
+          for(int i = 0 ; i < 3 ; i++)
+           {
+              for(int j = 0 ; j < 3 ; j++)
+               {
+                 Res.Arr[i][j] = Arr[i][j] + Obj.Arr[i][j];
+               }
+           }
+      return Res;
+    }
+
+    Matrix operator -(Matrix &Obj)
+    {
+      Matrix Res;
+          for(int i = 0 ; i < 3 ; i++)
+           {
+              for(int j = 0 ; j < 3 ; j++)
+               {
+                 Res.Arr[i][j] = Arr[i][j] - Obj.Arr[i][j];
+               }
+           }
+      return Res;
+    }
+
+    Matrix operator -()
+    {
+      Matrix Res;
+          for(int i = 0 ; i < 3 ; i++)
+           {
+              for(int j = 0 ; j < 3 ; j++)
+               {
+                 Res.Arr[i][j] = -1 * Arr[i][j];
+               }
+           }
+      return Res;
+    }
+
+    Matrix operator ++()
+    {
+      Matrix Res;
+          for(int i = 0 ; i < 3 ; i++)
+           {
+              for(int j = 0 ; j < 3 ; j++)
+               {
+                 Res.Arr[i][j] = ++Arr[i][j];
+               }
+           }
+      return Res;
+    }
+
+    Matrix operator --()
+    {
+      Matrix Res;
+          for(int i = 0 ; i < 3 ; i++)
+           {
+              for(int j = 0 ; j < 3 ; j++)
+               {
+                 Res.Arr[i][j] = --Arr[i][j];
+               }
+           }
+      return Res;
+    }
+
+    Matrix operator --(int)
+    {
+      Matrix Res;
+          for(int i = 0 ; i < 3 ; i++)
+           {
+              for(int j = 0 ; j < 3 ; j++)
+               {
+                 Res.Arr[i][j] = Arr[i][j]--;
+               }
+           }
+      return Res;
+    }
+
+    Matrix operator ++(int)
+    {
+      Matrix Res;
+          for(int i = 0 ; i < 3 ; i++)
+           {
+              for(int j = 0 ; j < 3 ; j++)
+               {
+                 Res.Arr[i][j] = Arr[i][j]++;
+               }
+           }
+      return Res;
+    }
 
 };
 
+void Menu()
+{
+    int Choice;
+    Matrix m1, m2, Res;
+    cout << "\nEnter The First Matrix:-\n";
+    m1.readMatrix();
+    cout << "\nEnter The Second Matrix:-\n";
+    m2.readMatrix();
+      while(Choice != 8)
+        {
+            cout << "\nChoose Any One Of The Following Choices :-"
+                 << "\n1. Addition"
+                 << "\n2. Subtraction"
+                 << "\n3. Negation"
+                 << "\n4. Pre-Incrementation"
+                 << "\n5. Post-Incrementation"
+                 << "\n6. Pre-Decrementation"
+                 << "\n7. Post-Decrementation"
+                 << "\n8. Exit"
+                 << "\nYour Choice Is : ";
+            cin >> Choice;
+            cout << endl;
+              switch(Choice)
+                {
+                    case 1 : Res = m1 + m2;
+                              break;
+
+                    case 2 : Res = m1 - m2;
+                              break;
+
+                    case 3 : Res = -m1;
+                              break;
+
+                    case 4 : Res = ++m1;
+                              break;
+
+                    case 5 : Res = m1++;
+                              break;
+
+                    case 6 : Res = --m1;
+                              break;
+
+                    case 7 : Res = m1--;
+                              break;
+
+                    case 8 : exit(0);
+                              break;
+
+                  default : cout << "\nInvalid Choice.\n";
+                }
+            Res.printMatrix();
+        }
+}
 int main(void)
 {
-        Array Obj;
-        int iArr[5];
-        float fArr[5];
-        Obj.calcSum(iArr);
-        Obj.calcSum(fArr);
-        cout << endl;
-        return 0;
+    Menu();
+    return 0;
 }
