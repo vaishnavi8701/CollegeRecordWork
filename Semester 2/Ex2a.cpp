70eff5f35171a88b190d566f75ca9fe99999c9f3
Nilesh2000 <nileshlund@gmail.com>
2019-03-29 09:54:33 +0530

    Update Ex2a.cpp

--- Semester 2/Ex2a.cpp
+++ Semester 2/Ex2a.cpp
@@ -1,47 +1,92 @@
 # include <iostream>
+# include <string>
 
 using namespace std;
 
-class Add2;
-
-class Add1
+class Car
 {
-    int a, b;
+    int Cost;
+    string Make, Color;
+    static int Id;
+
   public:
-    friend int addNum(Add1, Add2);
-    void getNum1()
-    {
-        cout << "\nEnter Two Numbers : ";
-        cin >> a >> b;
-    }
+
+      Car()
+      {
+          Cost = 200000;
+          Make = "Hyundai";
+          Color = "Sky Blue";
+      }
+
+      void getData()
+      {
+          cout << "\nEnter The Car Make : ";
+          cin >> Make;
+          cout << "Enter The Color : ";
+          cin >> Color;
+          cout << "Enter The Car Cost : ";
+          cin >> Cost;
+      }
+
+      void putData()
+      {
+          cout << "\nCar ID : " << Id
+               << "\nCar Make : " << Make
+               << "\nCar Color : " << Color
+               << "\nCar Cost : " << Cost
+               << "\n";
+      }
+
+      class carUpdate
+      {
+          int Amount;
+        public:
+          void Update(Car &Obj)
+          {
+              cout << "\nEnter The Amount To Be Incremented : ";
+              cin >> Amount;
+              Obj.Cost += Amount;
+          }
+      };
+
+      static void IDincrement()
+      {
+          Id++;
+      }
+
+      friend void Modify(Car &c);
 };
 
-class Add2
-{
-    int c, d;
-  public:
-    friend int addNum(Add1, Add2);
-    void getNum2()
-    {
-        cout << "Enter Two Numbers : ";
-        cin >> c >> d;
-    }
-};
+int Car::Id = 100;
 
-int addNum(Add1 Obj1, Add2 Obj2)
+void Modify(Car &c)
 {
-   int Sum1 = Obj1.a + Obj1.b;
-   int Sum2 = Obj2.c + Obj2.d;
-   return Sum1 + Sum2;
+  cout << "\nEnter The Car Make : ";
+  cin >> c.Make;
+  cout << "Enter The Color : ";
+  cin >> c.Color;
+  cout << "Enter The Car Cost : ";
+  cin >> c.Cost;
 }
 
 int main(void)
 {
-   Add1 a1;
-   Add2 a2;
-   a1.getNum1();
-   a2.getNum2();
-   int Sum = addNum(a1, a2);
-   cout << "The sum of the numbers is : " << Sum << endl;
-   return 0;
+    Car Obj, Obj1, Obj2;
+    Obj.putData();
+    cout << "\nCar 1:-\n";
+    Obj1.getData();
+    Car::IDincrement();
+    Obj1.putData();
+    cout << "\nCar 2:-\n";
+    Obj2.getData();
+    Car::IDincrement();
+    Obj2.putData();
+    Car::carUpdate Obj3;
+    cout << "\nUpdated Cost for Car 1:-\n";
+    Obj3.Update(Obj1);
+    Obj1.putData();
+    cout << "\nEnter modified detail for Car 2:-\n";
+    Modify(Obj2);
+    Obj2.putData();
+    return 0;
 }
