c5abae5ea9eb7630d8fafa0fdce80746adaaea4c
Nilesh D <nileshlund@gmail.com>
2019-07-13 12:55:24 +0530

    Added comments to signify questions.

--- Semester 3/Database Systems/Ex1 - Solutions.sql
+++ Semester 3/Database Systems/Ex1 - Solutions.sql
@@ -1,3 +1,4 @@
+--1. Table Name : EMPLOYEE
 create table EMPLOYEE(SSN int primary key, FNAME varchar(10) not null, MINT varchar(1) not null,
                       LNAME varchar(10) not null, BDATE datetime not null, ADDRESS varchar(10) not null,
 		                  SEX varchar(1) not null, SALARY int not null, SUPERSSN int, DNO int not null)
@@ -11,14 +12,14 @@ insert into EMPLOYEE values(453453453,'Joyce','A','English','31-jul-72','Houston
 insert into EMPLOYEE values(987987987,'Ahmad','V','Jebbar','29-mar-69','Houston','M',25000,987654321,4)
 insert into EMPLOYEE values(888665555,'James','E','Borg','10-nov-37','Houston','M',55000,NULL,1)
 
-
+--2. Table Name : DEPARTMENT
 create table DEPARTMENT(DINT int primary key, DNAME varchar(20) not null, MGRSSN int not null, MGRSTARTDATE datetime not null)
 
 insert into DEPARTMENT values(5,'Research',333445555,'22-may-88')
 insert into DEPARTMENT values(4, 'Administration',987654321,'01-jan-95')
 insert into DEPARTMENT values(1,'Headquarters',888665555,'19-june-81')
 
-
+--3. Table Name : DEPT_LOCATIONS
 create table DEPT_LOCATIONS(DINT int, DLOCATION varchar(10), primary key(DINT, DLOCATION))
 
 insert into DEPT_LOCATIONS values(1, 'Houston')
@@ -27,7 +28,7 @@ insert into DEPT_LOCATIONS values(5, 'Bellaire')
 insert into DEPT_LOCATIONS values(5, 'Sugarland')
 insert into DEPT_LOCATIONS values(5, 'Houston')
 
-
+--5. Table Name : WORKS_ON
 create table WORKS_ON(ESSN int, PNO int, HOURS numeric(3,1), primary key(ESSN,PNO))
 
 insert into WORKS_ON values(123456789,1,32.5)
@@ -47,7 +48,7 @@ insert into WORKS_ON values(987654321,30,20.0)
 insert into WORKS_ON values(987654321,20,15.0)
 insert into WORKS_ON values(888665555,20,NULL)
 
-
+--6. Table Name : DEPENDENT
 create table DEPENDENT(ESSN int, DEPENDENT_NAME varchar(10), SEX varchar(10) not null, BDATE datetime, RELATIONSHIP varchar(10) not null,
 		                   primary key(ESSN, DEPENDENT_NAME))
 
@@ -59,7 +60,7 @@ insert into DEPENDENT values(123456789,'Micheal','M','1988-01-04','SON')
 insert into DEPENDENT values(123456789,'Alice','F','1988-12-30','DAUGHTER')
 insert into DEPENDENT values(123456789,'Elizabeth','F','1967-05-05','SPOUSE')
 
-
+--8. Table Name : STUDENT
 create table STUDENT(SNO int, SNAME varchar(10), AGE int)
 
 insert into STUDENT values(01,'Theodore',16)
