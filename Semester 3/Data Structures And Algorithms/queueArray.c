ed49635908750b522329237282fa26899797d994
Nilesh D <nileshlund@gmail.com>
2019-09-04 13:58:11 +0530

    Create queueArray.c

---
+++ Semester 3/Data Structures And Algorithms/queueArray.c
@@ -0,0 +1,86 @@
+# include <stdio.h>
+# define SIZE 3
+
+int Queue[SIZE];
+int Front = 0, Rear = 0;
+
+void enqueueElement(int Val)
+{
+  if(Rear == SIZE)
+  {
+     printf("Queue is full. Insertion not possible.\n");
+     return ;
+  }
+
+  Queue[Rear] = Val;
+  Rear++;
+  printf("%d has been inserted into the queue.\n", Val);
+}
+
+void dequeueElement()
+{
+  if(Front == Rear)
+  {
+     printf("\nQueue is empty. Deletion not possible.\n");
+     return ;
+  }
+
+  Rear--;
+  printf("\n%d has been dequeued.\n", Queue[Rear]);
+}
+
+void displayQueue()
+{
+  if(Front == Rear)
+  {
+     printf("\nQueue is empty. No elements to display.\n");
+     return ;
+  }
+
+  int i;
+  printf("\nThe elements of the queue are : ");
+  for(i = Front ; i < Rear ; i++)
+    printf("%d ", Queue[i]);
+  printf("\n");
+}
+
+void Menu()
+{
+  int Choice = 0;
+  while(Choice != 4)
+  {
+    printf("\nEnter Any One Of The Following Choices:-"
+           "\n1. Enqueue A Element"
+           "\n2. Dequeue A Element"
+           "\n3. Display The Queue"
+           "\n4. Exit"
+           "\nYour choice is : ");
+    scanf("%d", &Choice);
+    switch(Choice)
+    {
+        case 1 : {
+                  int Data;
+                  printf("\nEnter the element : ");
+                  scanf("%d", &Data);
+                  enqueueElement(Data);
+                   break;
+                 }
+
+        case 2 : dequeueElement();
+                  break;
+
+        case 3 : displayQueue();
+                  break;
+
+        case 4 :  break;
+
+        default : printf("\nInvalid Choice.\n");
+    }
+  }
+}
+
+int main(void)
+{
+  Menu();
+  return 0;
+}
