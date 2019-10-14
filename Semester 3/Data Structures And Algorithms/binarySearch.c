33fbfb3415c8e2e5da34472c71b7e18a5e49ef07
Nilesh D <nileshlund@gmail.com>
2019-10-14 10:16:51 +0530

    Update binarySearch.c

--- Semester 3/Data Structures And Algorithms/binarySearch.c
+++ Semester 3/Data Structures And Algorithms/binarySearch.c
@@ -8,7 +8,7 @@ void binarySearch(int Arr[], int n, int x)
           Mid = (Low + High) / 2;
             if(x == Arr[Mid])
               {
-                  printf("\n%d found at position %d", x, Mid + 1);
+                  printf("\n%d found at position %d\n", x, Mid + 1);
                   Flag = 1;
                    break;
               }
@@ -23,15 +23,20 @@ void binarySearch(int Arr[], int n, int x)
         }
     if(Flag == 0)
     {
-      printf("\n%d is not present in the array.", x);
+      printf("\n%d is not present in the array.\n", x);
     }
 }
 
-void main()
+int main()
 {
-    int Array[] = {19, 22, 24, 47, 47, 53, 61};
-    int Size = sizeof(Array) / sizeof(Array[0]), Search;
-    int firstSearch = 53, secondSearch = 92;
-    binarySearch(Array, Size, firstSearch);
-    binarySearch(Array, Size, secondSearch);
+    int Arr[100], n, i, Num;
+    printf("\nEnter the size of the array : ");
+    scanf("%d", &n);
+    printf("Enter the elements of the array (In Sorted Order) : ");
+    for(i = 0 ; i < n ; i++)
+     scanf("%d", &Arr[i]);
+    printf("Enter element for search : ");
+    scanf("%d", &Num);
+    binarySearch(Arr, n, Num);
+    return 0;
 }
