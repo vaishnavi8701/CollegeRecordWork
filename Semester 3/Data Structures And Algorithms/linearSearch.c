8750743dd2fa21c034f1e1a8c1837d4fa5ff94fc
Nilesh D <nileshlund@gmail.com>
2019-10-14 10:19:02 +0530

    Update linearSearch.c

--- Semester 3/Data Structures And Algorithms/linearSearch.c
+++ Semester 3/Data Structures And Algorithms/linearSearch.c
@@ -7,26 +7,25 @@ void linearSearch(int Arr[], int n, int Num)
   {
       if(Arr[i] == Num)
       {
-          printf("\n%d is found at position %d.", Num, i + 1);
+          printf("\n%d is found at position %d.\n", Num, i + 1);
           Flag = 1;
            break;
       }
   }
   if(Flag == 0)
-    printf("\n%d is not present in the array.", Num);
+    printf("\n%d is not present in the array.\n", Num);
 }
 
 int main(void)
 {
-    int Arr[] = {1, 3, 5, 2, 6, 8, 9, 4};
-    int n = sizeof(Arr) / sizeof(Arr[0]);
-    int Num1 = 6, Num2 = 7;
-    linearSearch(Arr, n, Num1);
-    linearSearch(Arr, n, Num2);
+    int Arr[100], n, i, Num;
+    printf("\nEnter the size of the array : ");
+    scanf("%d", &n);
+    printf("Enter the elements of the array : ");
+    for(i = 0 ; i < n ; i++)
+     scanf("%d", &Arr[i]);
+    printf("Enter element for search : ");
+    scanf("%d", &Num);
+    linearSearch(Arr, n, Num);
     return 0;
 }
-
-/*
-6 is found at position 5.
-7 is not present in the array.
-*/
