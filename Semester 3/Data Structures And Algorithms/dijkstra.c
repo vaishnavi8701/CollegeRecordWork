91d3eecaaaee08ab435d052a7c4f4ebc79f60224
Nilesh D <nileshlund@gmail.com>
2019-10-14 19:08:54 +0530

    Improved code, but algorithm not working.

--- Semester 3/Data Structures And Algorithms/dijkstra.c
+++ Semester 3/Data Structures And Algorithms/dijkstra.c
@@ -1,47 +1,55 @@
-#include <stdio.h>
-#define infinity 999
+# include <stdio.h>
+# define INF 9999
 
-void dij(int n,int v,int cost[10][10],int dist[])
+void singleSourceShortestPath(int n, int v, int Graph[][10], int Dist[])
 {
- int i,u,count,w,flag[10],min;
- for(i=1;i<=n;i++)
-  flag[i]=0,dist[i]=cost[v][i];
- count=2;
- while(count<=n)
- {
-  min=99;
-  for(w=1;w<=n;w++)
-   if(dist[w]<min && !flag[w])
-    min=dist[w],u=w;
-  flag[u]=1;
-  count++;
-  for(w=1;w<=n;w++)
-   if((dist[u]+cost[u][w]<dist[w]) && !flag[w])
-    dist[w]=dist[u]+cost[u][w];
- }
+  int i,u,count,w,flag[10],min;
+for(i=0;i<n;i++)
+flag[i]=0,Dist[i]=Graph[v][i];
+count=2;
+while(count<=n)
+{
+min=99;
+for(w=0;w<n;w++)
+ if(Dist[w]<min && !flag[w])
+  min=Dist[w],u=w;
+flag[u]=1;
+count++;
+for(w=0;w<n;w++)
+ if((Dist[u]+Graph[u][w]<Dist[w]) && !flag[w])
+  Dist[w]=Dist[u]+Graph[u][w];
+}
 }
 
-void main()
+int main(void)
 {
- int n,v,i,j,cost[10][10],dist[10];
+    int n;
+    printf("\nEnter the number of nodes : ");
+    scanf("%d", &n);
+    int Graph[n][n], Dist[n];
+    printf("\nEnter the distance matrix : \n");
 
- printf("\n Enter the number of nodes:");
- scanf("%d",&n);
- printf("\n Enter the cost matrix:\n");
- for(i=1;i<=n;i++)
-  for(j=1;j<=n;j++)
-  {
-   printf("dist[%d][%d]=", i, j);
-   scanf("%d",&cost[i][j]);
-   if(cost[i][j]==0)
-    cost[i][j]=infinity;
-  }
- printf("Enter the source node : ");
- scanf("%d",&v);
- dij(n,v,cost,dist);
- printf("\nShortest path:");
- for(i=1;i<=n;i++)
- if(i!=v)
-   printf("\nNode %d -> Node %d,cost=%d\n",v,i,dist[i]);
+    for(int i = 0 ; i < n ; i++)
+    {
+        for(int j = 0 ; j < n ; j++)
+        {
+            printf("Distance[%d][%d] = ", i, j);
+            scanf("%d", &Graph[i][j]);
+            if(Graph[i][j] == 0)
+              Graph[i][j] = INF;
+        }
+    }
+
+    int v;
+    printf("\nEnter the source node : ");
+    scanf("%d", &v);
+    singleSourceShortestPath(n, v, Graph, Dist);
+    for(int i = 0 ; i < n ; i++)
+    {
+        if(i != v)
+        {
+          printf("\n%d -> %d", i, Dist[i]);
+        }
+    }
+    return 0;
 }
-
--- Semester 3/Data Structures And Algorithms/floydWarshall.c
+++ Semester 3/Data Structures And Algorithms/floydWarshall.c
@@ -1,32 +1,47 @@
 #include<stdio.h>
-int i, j, k,n,dist[10][10];
-void floydWarshall ()
+
+void allSourceShortestPath(int n, int Graph[][10], int Dist[][10])
 {
- for (k = 0; k < n; k++)
-  for (i = 0; i < n; i++)
-   for (j = 0; j < n; j++)
-    if (dist[i][k] + dist[k][j] < dist[i][j])
-     dist[i][j] = dist[i][k] + dist[k][j];
+  for(int k = 0; k < n; k++)
+  {
+    for (int i = 0; i < n; i++)
+    {
+      for (int j = 0; j < n; j++)
+      {
+        if (Dist[i][k] + Dist[k][j] < Dist[i][j])
+        {
+          Dist[i][j] = Dist[i][k] + Dist[k][j];
+        }
+      }
+    }
+  }
 }
-int main()
+
+int main(void)
 {
-  int i,j;
-  printf("Enter no of vertices :");
-  scanf("%d",&n);
-  printf("\n");
-  for(i=0;i<n;i++)
-  for(j=0;j<n;j++)
-   {
-    printf("dist[%d][%d]:",i,j);
-    scanf("%d",&dist[i][j]);
-   }
- floydWarshall();
- printf (" \n\n shortest distances between every pair of vertices \n");
- for (int i = 0; i < n; i++)
- {
-  for (int j = 0; j < n; j++)
-   printf ("%d\t", dist[i][j]);
-  printf("\n");
- }
- return 0;
+  int n;
+  printf("Enter number of nodes : ");
+  scanf("%d", &n);
+  int Graph[n][n], Dist[n][n];
+
+  for(int i = 0; i < n ; i++)
+  {
+    for(int j = 0 ; j < n ; j++)
+    {
+      printf("Distance[%d][%d] = ", i, j);
+      scanf("%d", &Graph[i][j]);
+    }
+  }
+
+  allSourceShortestPath(n, Graph, Dist);
+
+  for(int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < n; j++)
+    {
+      printf ("\n%d\t", Dist[i][j]);
+    }
+  }
+
+  return 0;
 }
