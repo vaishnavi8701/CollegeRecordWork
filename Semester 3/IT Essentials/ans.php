9d42bbca60ce7dc70a731a161f39f873fe86ffc3
Nilesh D <nileshlund@gmail.com>
2019-10-31 20:09:18 +0530

    Create ans.php

---
+++ Semester 3/IT Essentials/ans.php
@@ -0,0 +1,28 @@
+<?php
+        if(isset($_POST['num']))
+	{
+                $n = $_POST['num'];
+                $temp = $n;
+                $sum = 0;
+                $count = 0;
+                while($temp > 0)
+                {
+                    $count++;
+                    $temp = (int)($temp / 10); 
+                }
+                $temp = $n;
+                while($temp > 0){
+                    $rem = $temp % 10;
+                    $sum = $sum + pow($rem, $count);
+                    $temp = (int)($temp / 10);
+                }
+                if($sum == $n)
+                {
+                    echo 'Given number is Armstrong';
+                }
+          	else
+                {
+                    echo 'Not an Armstrong number';
+                }
+        }
+?>
