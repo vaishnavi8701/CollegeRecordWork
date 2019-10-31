a3c9ceb9996537bc0cc48238e17b86bc50d6a9c1
Nilesh D <nileshlund@gmail.com>
2019-10-31 20:12:25 +0530

    Update new.php

--- Semester 3/IT Essentials/new.php
+++ Semester 3/IT Essentials/new.php
@@ -8,7 +8,7 @@
 <body>
     <h2>Armstrong Number</h2>
     <form action="ans.php" method="POST">
-        <input type="number" name="num">
+        <input type="number" name="num" required>
         <input type="submit" value="Submit" name="req">
     </form>
 </body>
