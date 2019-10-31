316db30c4ba92f57d727a08886d1181e2e18a1f6
Nilesh D <nileshlund@gmail.com>
2019-10-31 20:09:52 +0530

    Create new.php

---
+++ Semester 3/IT Essentials/new.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Armstrong Number</title>
+</head>
+
+<body>
+    <h2>Armstrong Number</h2>
+    <form action="ans.php" method="POST">
+        <input type="number" name="num">
+        <input type="submit" value="Submit" name="req">
+    </form>
+</body>
+
+</html>
